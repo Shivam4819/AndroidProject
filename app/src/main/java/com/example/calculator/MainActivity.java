951f863d9f73f22262b352da3f9361f1106bf171
shivam4819 <shivam4819@gmail.com>
2021-01-08 11:58:46 +0530

    first commit

---
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+
+
+}
